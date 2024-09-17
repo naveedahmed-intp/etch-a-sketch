dac69816406f08f8cfad9e484c8866e9f53f8c50
naveedahmed-intp <dc230400318nak@vu.edu.pk>
2024-09-17 21:05:00 +0500

    Add a canvas with white background and blue border
    When hover over the canvas, background color of the canvas grid elements will change randomly. Default size of canvas elements is 16 x 16 but can be changes from 1 to 100.

--- javascript/eas.js
+++ javascript/eas.js
@@ -1,8 +1,6 @@
 let container = document.querySelector("#container");
 let button = document.querySelector("#btn");
 
-let divs = [];
-
 function bg_Color() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -12,22 +10,50 @@ function bg_Color() {
     return bgColor;
 }
 
-let body = document.querySelector("body");
-let newRandomColor = Math.floor(Math.random()*16777215).toString(16);
-
 window.addEventListener('load', () => {
-    let GridSize = 16;
-    if (GridSize > 0 && GridSize <= 100) {
-        for(let i = 1; i <= Math.pow(GridSize, 2); i++) {
+    let defaultGridSize = 16;
+    makeGrid(defaultGridSize);
+});
+
+button.addEventListener('click', () => {
+    while(true) {
+        let gridSize = prompt("Enter Grid Size (1-100)");
+        if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) 
+            {
+                clearGrid();
+                makeGrid(gridSize);
+                break;
+        }
+    }
+})
+
+let boxes = document.querySelectorAll(".box");
+
+container.addEventListener('mouseover', (e) => {
+    if(e.target.classList.contains("box")) {
+        e.target.style.backgroundColor = bg_Color();
+    }
+})
+
+function makeGrid(size) {
+    if (size > 0 && size <= 100) {
+        for(let i = 1; i <= Math.pow(size, 2); i++) {
             let div = document.createElement("div");
-            div.style.padding = (960/(2*GridSize)) + "px";
-            div.classList.add("box");
-            div.style.backgroundColor = bg_Color();
+            div.className = "box";
+            div.style.padding = (960/(2*size)) + "px";
+            //div.style.backgroundColor = bg_Color();
             div.style.justifyContent = "space-between";
             container.appendChild(div);
         }
     }
-})
+}
+
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
 
 
 
